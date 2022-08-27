1b4e5cb7a4adec029a998533b37c9503d5a7e5c6
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-08-26 20:25:44 -0400

    Applied flexbox for youtube videos
    Used flexbox to make sure that demo videos are not to go over the page when the screen size changes.

--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -4,10 +4,23 @@ import YoutubeEmbed from "./youtubeEmbed";
 function Videos(){
     return(
         <div className="demoVideoSection">
+            <h1>Portfolio Showcase</h1>
             <div className="demoVideos">
-                <YoutubeEmbed embedId="QcDN3Q0TgCU"/>
-                <YoutubeEmbed embedId="q6O9js6K6sY"/>
-                <YoutubeEmbed embedId="d6Tzqg8BWzc"/>
+                <div className="first-video">
+                    <h3>GD Watcher</h3>
+                    <YoutubeEmbed embedId="QcDN3Q0TgCU"/>
+                    <a href=""></a>
+                </div>
+                <div className="second-video">
+                    <h3>Bloggy</h3>
+                    <YoutubeEmbed embedId="q6O9js6K6sY"/>
+                    <a href=""></a>
+                </div>
+                <div className="third-video">
+                    <h3>Ez Weather</h3>
+                    <YoutubeEmbed embedId="d6Tzqg8BWzc"/>
+                    <a href=""></a>
+                </div>
             </div>
         </div>
     )
