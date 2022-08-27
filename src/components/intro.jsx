610fa7194796c5387d652041efedbfa8e3715140
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-08-27 12:26:53 -0400

    Updated CSS and links
    Aligned all videos to center and created links to my github, linkedin, and medium profile.

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -11,6 +11,12 @@ function Intro(){
             <div className="intro">
                 <span className="content">I'm a Software Engineer in New York. I enjoy creating fun stuffs on web and I worked on projects involving frontend and backend.</span>
             </div>
+
+            <div className="links">
+                <a href="https://github.com/jeffkim1118" className="githubLink" id="linkURL">Github</a>
+                <a href="https://www.linkedin.com/in/yoonsung-kim-639b30178/" className="linkedinLink" id="linkURL">Linkedin</a>
+                <a href="https://medium.com/@1019yskim" className="mediumLink" id="linkURL">Medium Blog</a>
+            </div>
         </div>
     )
 }
--- src/components/videos.jsx
+++ src/components/videos.jsx
@@ -6,19 +6,19 @@ function Videos(){
         <div className="demoVideoSection">
             <h1>Portfolio Showcase</h1>
             <div className="demoVideos">
-                <div className="first-video">
+                <div className="first-video" id="video">
                     <h3>GD Watcher</h3>
                     <YoutubeEmbed embedId="QcDN3Q0TgCU"/>
                     <p>Web app to display all video games deals in many online stores. Also, it creates watchlist to keep track of demanded video games to purchase.</p>
                     <a href="https://github.com/jeffkim1118/game-deals-watcher" target="_blank" rel="noreferrer">Github</a>
                 </div>
-                <div className="second-video">
+                <div className="second-video" id="video">
                     <h3>Bloggy</h3>
                     <YoutubeEmbed embedId="q6O9js6K6sY"/>
                     <p>This is a simple blogging website that used React and Rails</p>
                     <a href="https://github.com/jeffkim1118/blog-website" target="_blank" rel="noreferrer">Github</a>
                 </div>
-                <div className="third-video">
+                <div className="third-video" id="video">
                     <h3>Ez Weather</h3>
                     <YoutubeEmbed embedId="d6Tzqg8BWzc"/>
                     <span>A weather web application built with HTML5, CSS3, and JavaScript. It provides temperature and weather conditions of cities that are searched.</span>
