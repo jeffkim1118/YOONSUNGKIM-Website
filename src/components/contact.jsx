767a4193e6166d3975019c3f8aeacde7c54c5c8f
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-08-30 19:37:21 -0400

    Created contact.jsx file
    Created contact.jsx file to make a form to send me an email from users who visit my website.

---
+++ src/components/contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contact(){
+    return(
+        <div>
+            <form>
+                <input placeholder="Sender name"></input>
+                <input placeholder="Type your message"></input>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    )
+}
+export default Contact;
