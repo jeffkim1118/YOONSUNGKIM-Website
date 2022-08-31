23ed17b11d2ecfa6c51cc2316d284ff3355b30c4
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-08-31 19:21:30 -0400

    installed emailjs package to the project
    installed emailJs package to create a form to send an email from the website to my gmail address.

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 
 function Contact(){
+    const form = useRef();
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+          console.log(error.text);
+        });
+    }
+
     return(
         <div>
-            <form>
-                <input placeholder="Sender name"></input>
-                <input placeholder="Type your message"></input>
-                <button type="submit">Send</button>
+            <form ref={form} onSubmit={handleSubmit}>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
             </form>
         </div>
     )
