c8012126e35e8905b6b5c512b3ce36e236dc76ab
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-09-03 21:01:23 -0400

    Removed react router.
    Removed react router feature because netlify only supports single page application.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,18 @@ import Resume from './components/resume';
 import Footer from './components/footer';
 import Home from './components/Home';
 import Contact from './components/contact';
-import { Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className='app-container'>
-      <Routes> 
-        <Route path="/" element={<Home />}/>
-        <Route path="/bio" element={<Bio />}/>
-        <Route path="/portfolio" element={<Video/>}/>
-        <Route path="/resume" element={<Resume />}/>
-        <Route path="/contact" element={<Contact />}/>
-      </Routes>
+        <Home />
+        <Bio />
+        <Video />
+        <Resume />
+        <Contact />
       </div>
-  
       <Footer />
     </div>       
   );
