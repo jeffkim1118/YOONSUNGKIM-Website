fc68514ba9d0b38acfca356061edc950525f03c1
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-09-05 12:42:21 -0400

    Warning component added
    Warning component added to check if all the inputs are not empty in the contact form.

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,31 +1,41 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import emailjs from '@emailjs/browser';
+import Warning from "./warning";
 
 function Contact(){
+    const[name, setName] = useState("");
+    const[email, setEmail] = useState("");
+    const[message, setMessage] = useState("");
+    const[status, setStatus] = useState(true);
+
     const form = useRef();
 
     function handleSubmit(e){
         e.preventDefault();
-
-        emailjs.sendForm('service_qufbpuo', 'template_4y8qqhm', form.current, 'BYtwJwTOulJN4yq_L')
-        .then((result) => {
-            console.log(result.text);
-            e.target.reset();
-            console.log("message sent");
-        }, (error) => {
-          console.log(error.text);
-        });
+        if(name === "" || email === "" || message === ""){
+            setStatus(false);
+        }else{
+            emailjs.sendForm('service_qufbpuo', 'template_4y8qqhm', form.current, 'BYtwJwTOulJN4yq_L')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset();
+                console.log("message sent");
+            }, (error) => {
+            console.log(error.text);
+            });
+        }
     }
 
     return(
         <div className="email-form-container">
+            {!status ? <Warning /> : null}
             <form ref={form} onSubmit={handleSubmit}>
                 <label>Name</label>
-                <input type="text" name="user_name" />
+                <input type="text" name="user_name" value={name} onChange={(e) => setName(e.target.value)}/>
                 <label>Email</label>
-                <input type="email" name="user_email" />
+                <input type="email" name="user_email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <label>Message</label>
-                <textarea name="message" />
+                <textarea name="message" value={message} onChange={(e)=>setMessage(e.target.value)}/>
                 <input type="submit" value="Send" />
             </form>
         </div>
---
+++ src/components/warning.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Warning(){
+    return(
+        <div>
+            <p>Error: Please make sure you entered all the necessary inputs!</p>
+        </div>
+    )
+}
+export default Warning;
