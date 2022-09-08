3e8fbcd8a9472f39f392f611c65c7992016b671a
Yoonsung Kim <64029918+jeffkim1118@users.noreply.github.com>
2022-09-07 23:02:36 -0400

    Warning message changed

--- src/components/warning.jsx
+++ src/components/warning.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Warning(){
     return(
         <div class="alert alert-warning" role="alert">
-            <p>Error: Please make sure you entered all the necessary inputs!</p>
+            <p>Please make sure you entered all the necessary inputs!</p>
         </div>
     )
 }
